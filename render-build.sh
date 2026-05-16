3abdcc00abdd15b174488e3dd02baa525b600667
jrpaiva <91551934+jrpaiva@users.noreply.github.com>
2026-05-16 01:28:42 +0000

    1

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Instala as dependências normais do projeto
-npm install
-
-# Força o download do navegador Chrome oficial do Puppeteer para a nuvem
-npx puppeteer browsers install chrome
